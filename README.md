e0b2089b9d48a7c07088c15499c9974f11b53230
atulya-tejas <atulyatejas@gmail.com>
2026-01-02 22:02:24 +0530

    Done Example 2 to Example 4

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Learner
 learning DOM and BOM manupulation 
-
-example 1. change the paragraph when the button is clicked. 
+<br><br>
+example 1. Accessing DOM Elements. Accessing the text of paragraph and changeing the  paragraph text when the button is clicked. 
+<br><br>
+example 2. Traversing the DOM . adding a highlighting class to the 1st element ( tokyo) of the unorder list . 
+<br><br>
+example 3. Manipulating DOM Elements. Changing the order from latte to Espresso.
+<br><br>
+example 4. Creating and Inserting Elements. Creating a new virtual element (egg) in js and injecting it in the shopping list as a child node , every time the button is clicked . 
--- script.js
+++ script.js
@@ -4,3 +4,29 @@ document.getElementById("changeTextButton").addEventListener("click",function(){
     let paragraph = document.getElementById("myParagraph");
     paragraph.textContent = "This is the changed the paragraph";
 })
+
+//example 2 
+
+document.getElementById("highlightFirstCity").addEventListener("click",function(){
+    let cities = document.getElementById("citiesList");
+    cities.firstElementChild.classList.add("highlight");
+    
+})
+
+//example 3
+
+document.getElementById("changeOrder").addEventListener("click",function(){
+    let order = document.getElementById("coffeeType") ;
+    order.textContent = "Espresso";
+    order.style.color = "pink";
+    order.style.backgroundColor = "red";
+    order.style.padding = "5px";
+})
+
+//example 4
+
+document.getElementById("addNewItem").addEventListener("click",function(){
+    let newItem = document.createElement("li");
+    newItem.textContent = "Eggs";
+    document.getElementById("shoppingList").appendChild(newItem);
+})
